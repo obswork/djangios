67826c49a675b701218c2adf336aee60ede8f1b8
obswork <obssabizuwork@gmail.com>
2016-07-16 16:39:00 -0400

    added view, template, routes to display most recent node_status

--- collector/views.py
+++ collector/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from collector.models import DataPoint
 
-# Create your views here.
+
+class StatusView(TemplateView):
+    template_name = 'status.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(StatusView, self).get_context_data(**kwargs)
+
+        # to optimize db queries - first find the unique node/data_type pairs
+        nodes_and_data_types = DataPoint.objects.all().values('node_name', 'data_type').distinct()
+
+        # dict of node--data-type--data-value to return to the the template
+        data_status = {}
+
+        # for each distinct node--data-type pair query the db for the latest DatePoint instance (status)
+        for node_and_data_type in nodes_and_data_types:
+            # node_name and data_type to filter query by
+            node_name = node_and_data_type['node_name']
+            data_type = node_and_data_type['data_type']
+
+            # retrive/set dict w/ key:val = node_name: {} and
+            # store in data_point_map var
+            data_point_map = data_status.setdefault(node_name, dict())
+            # for each data_type, for each node, store the latest value
+            data_point_map[data_type] = DataPoint.objects.filter(
+                node_name=node_name, data_type=data_type
+            ).latest('datetime')
+
+        context['data_status'] = data_status
+
+        return context
--- config/urls.py
+++ config/urls.py
@@ -7,9 +7,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from collector.views import StatusView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    url(r'^home/$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
+    url(r'^$', StatusView.as_view(), name='status'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
     # Django Admin, use {% url 'admin:index' %}
