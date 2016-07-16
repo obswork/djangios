d98c8701a894e001aa6dabe6f0c2828a4ec3d4bc
obswork <obssabizuwork@gmail.com>
2016-07-16 17:43:47 -0400

    added views, templates, routes to edit/add alerts w/ link on alerts listview

--- notifier/views.py
+++ notifier/views.py
@@ -1,7 +1,30 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView
+from django.core.urlresolvers import reverse
 from notifier.models import Alert
 
 
 class AlertListView(ListView):
     template_name = 'alerts_list.html'
     model = Alert
+
+
+class NewAlertView(CreateView):
+    template_name = 'create_or_update_alert.html'
+    model = Alert
+    fields = [
+        'node_name', 'data_type', 'max_value', 'min_value', 'active'
+    ]
+
+    def get_success_url(self):
+        return reverse('alerts-list')
+
+
+class EditAlertView(UpdateView):
+    template_name = 'create_or_update_alert.html'
+    model = Alert
+    fields = [
+        'node_name', 'data_type', 'max_value', 'min_value', 'active'
+    ]
+
+    def get_success_url(self):
+        return reverse('alerts-list')
