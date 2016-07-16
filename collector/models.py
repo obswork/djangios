2300c60dc880f2f1944da111e68931d2e993e088
obswork <obssabizuwork@gmail.com>
2016-07-16 08:53:09 -0400

    added point collection model

--- collector/models.py
+++ collector/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class DataPoint(models.Model):
+    """Stores client info with datetime, type, and value of data point collected
+    """
+
+    node_name = models.CharField(max_length=255)
+    datetime = models.DateTimeField(auto_now_add=True)
+    data_type = models.CharField(max_length=100)
+    data_value = models.FloatField()
